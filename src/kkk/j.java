dbba1308ee6750232a48d5a563782fc89b72fef5
Boxstop <icedroidyt@gmail.com>
2021-04-11 20:00:14 +0300

    Java Test

---
+++ src/kkk/j.java
@@ -0,0 +1,12 @@
+package kkk;
+
+public class j {
+public static void main(String[] args) {
+	int[] array= {3,1,8,2,5};
+	array[3]=2;
+	int g=Math.max(4,2);
+	//array[4]=1+Math.max(array[0],array[1],array[3]);
+	String show=""+array.length+1;
+	System.out.println(g);
+}
+}
